e2d1b7cb903fc029e36be92d96d8142213fd1a54
emilrange <emilrange@gmail.com>
2015-03-18 20:01:15 +0100

    added gitignore. show offline and online img and load img when not yet got any status from irc

--- run.js
+++ run.js
@@ -5,6 +5,12 @@ var fs = require('fs');
 var config = false;
 var loadConfig = false;
 fs.readFile("config",{encoding: 'utf-8'},function(err,data){ config = eval("("+data+")"); loadConfig = true;});
+var img01 = fs.readFileSync("img/img01.png");
+var img02 = fs.readFileSync("img/img02.png");
+var imgload = fs.readFileSync("img/load.gif");
+
+
+
 
 // This is script is far from working. Just starts with the standard code for the moment// 
 
@@ -15,9 +21,9 @@ var getStartHtml = function()
 {
     var jq = '<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>';
     var ajax = 'setInterval(function(){$.ajax({url:"/status"}).done(function(data){'+
-    'var j = eval("("+data+")");'+'if(j.s==1) $("#status").html("online");'+'if(j.s==-1) $("#status").html("offline");'+
+    'var j = eval("("+data+")");'+'if(j.s==1) { $("#status").html("online"); $("#user_img").attr("src","img01.png");} '+'if(j.s==-1) { $("#status").html("offline"); $("#user_img").attr("src","img02.png"); } '+
     '})},1000)';
-    return jq+'<b id="status">.</b><script>$(document).ready(function(){'+ajax+"});</script>";
+    return jq+'<img id="user_img" src="load.gif" alt="user_img"><b id="status">.</b><script>$(document).ready(function(){'+ajax+"});</script>";
 }
 
 var getStatus = function()
@@ -47,7 +53,37 @@ http.createServer(function(req,res)
 
     if(url=="/")requestType=1;
     if(url=="/status")requestType=2;
-    
+    if(url=="/img01.png") requestType=3;
+    if(url=="/img02.png") requestType=4;
+    if(url=="/load.gif") requestType=5;
+    if(url=="/img04.png") requestType=6;
+
+    console.log(url);
+
+    if(requestType==3)
+    {
+        res.writeHead(200,{'Content-Type':'image/png'});
+        res.end(img01,'binary');
+    }
+
+    if(requestType==4)
+    {
+        res.writeHead(200,{'Content-Type':'image/png'});
+        res.end(img02,'binary');
+    }
+    if(requestType==5)
+    {
+        res.writeHead(200,{'Content-Type':'image/gif'});
+        res.end(imgload,'binary');
+
+
+    }
+    if(requestType==6)
+    {
+        throw "not yet added";
+    }
+
+
     if(requestType==1)
     {
         res.writeHead(200,{'Content-Type':'text/html'});
@@ -63,7 +99,8 @@ http.createServer(function(req,res)
 
 }).listen(config.http_port,config.http_accept_connect_on_hostname);
 
-
+if(true)
+{
 irc = new net.Socket();
 irc.connect(config.irc_port,config.irc_server,function(){});
 irc.on('data',function(data){
@@ -93,5 +130,6 @@ irc.on('data',function(data){
 irc.write("NICK "+config.check_online_nickname+"\r\n");
 irc.write("USER "+config.check_online_nickname+" 0 * :"+config.chech_online_nickname+"\r\n");
 setInterval(function(){ irc.write("ISON "+config.user_irc_nickname+"\r\n");},5000);
+}
 
 },2000);
