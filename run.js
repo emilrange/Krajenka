bcbb0fed6ce03b77428aeebc4481c3094f9ca47c
emilrange <emilrange@gmail.com>
2015-03-18 16:48:11 +0100

    added config. connects to irc. webpage tells if irc user is online or offline

--- run.js
+++ run.js
@@ -1,27 +1,44 @@
 var http = require('http');
+var net = require('net');
+var fs = require('fs');
+
+var config = false;
+var loadConfig = false;
+fs.readFile("config",{encoding: 'utf-8'},function(err,data){ config = eval("("+data+")"); loadConfig = true;});
 
 // This is script is far from working. Just starts with the standard code for the moment// 
 
-var port = "";
-var host = "";
-if(port=="") throw "";
-if(host=="") throw "";
 
+var online = null;
 
 var getStartHtml = function()
 {
     var jq = '<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>';
     var ajax = 'setInterval(function(){$.ajax({url:"/status"}).done(function(data){'+
-    'var j = eval("("+data+")");'+'if(j.s==1) $("#status").html("online");'+'if(j.s==0) $("#status").html("offline");'+
+    'var j = eval("("+data+")");'+'if(j.s==1) $("#status").html("online");'+'if(j.s==-1) $("#status").html("offline");'+
     '})},1000)';
-    return jq+'<b id="status">Online</b><script>$(document).ready(function(){'+ajax+"});</script>";
+    return jq+'<b id="status">.</b><script>$(document).ready(function(){'+ajax+"});</script>";
 }
 
 var getStatus = function()
 {
-    return "{s:"+parseInt(Math.random()*2)+"}";
+    var onlineStatus = 0;
+    if(online===true) onlineStatus = 1;
+    if(online===false) onlineStatus = -1;
+    return "{s:"+onlineStatus+"}";
 }
 
+setTimeout( function(){
+
+if(config===false) throw "config failed to load";
+if(loadConfig===false) throw "cofig failed to load";
+if(config.http_accept_connect_on_hostname=="") throw "invalid http_accept_connect_on_hostname";
+if(config.http_port=="") throw "invalid http_port";
+if(config.irc_port=="") throw "invalid irc_port";
+if(config.irc_server=="") throw "irc_server";
+if(config.check_online_nickname=="") throw "invalid check_online_nickname";
+if(config.user_irc_nickname=="") throw "invalid user_irc_nickname";
+ 
 http.createServer(function(req,res)
 {
 
@@ -44,4 +61,37 @@ http.createServer(function(req,res)
     }
 
 
-}).listen(port,host);
+}).listen(config.http_port,config.http_accept_connect_on_hostname);
+
+
+irc = new net.Socket();
+irc.connect(config.irc_port,config.irc_server,function(){});
+irc.on('data',function(data){
+    var dataStr = ""+data;
+
+    var messages = dataStr.split("\r\n");
+    
+    for(var i=0; i<messages.length; i++)
+    {
+        var msg = messages[i];
+        var parameters = msg.split(" ");
+
+        console.log(msg);
+        if(msg.substr(0,4)=="PING")
+        {
+            var parameters = msg.split(" ");
+            var t = "PONG "+parameters[1]+"\r\n";
+            irc.write(t);
+        }
+        if(parameters[1]=="303")
+        {
+            online = ( parameters[3] == ":"+config.user_irc_nickname );
+        }
+
+    }
+});
+irc.write("NICK "+config.check_online_nickname+"\r\n");
+irc.write("USER "+config.check_online_nickname+" 0 * :"+config.chech_online_nickname+"\r\n");
+setInterval(function(){ irc.write("ISON "+config.user_irc_nickname+"\r\n");},5000);
+
+},2000);
